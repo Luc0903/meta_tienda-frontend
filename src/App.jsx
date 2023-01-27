3350183248c7d0680ca58565fc835c7dab9ee9c8
Lucio Vasquez <castrocociolucio@gmail.com>
2023-01-27 17:52:20 -0300

    :zap: Oulet added to app.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { Outlet } from 'react-router-dom'
 import './App.css'
 
 function App() {
-  return 
+  <Outlet /> 
 }
 
 export default App
